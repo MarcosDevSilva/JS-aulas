ea64e4e0606404244670c3473b74e542f0f89d2d
Marco <tassio50000@gmail.com>
2024-12-11 22:03:26 -0300

    meus acertos e api mega loteria

--- megaSena.js
+++ megaSena.js
@@ -1,6 +1,7 @@
 let numerosSorteados = [];
 let meuJogo =[5,10,35,48,29,19];
 let numeroDeAcertos = 0;
+let numerosAcertados = [];
 
 
 for (let index = 0; index < 6; index += 1 ){
@@ -16,13 +17,24 @@ for (let indexSorteados = 0; indexSorteados < numerosSorteados.length; indexSort
     for ( let indexMeujogo = 0; indexMeujogo < meuJogo.length; indexMeujogo +=1){
        if (numerosSorteados[indexSorteados] === meuJogo[indexMeujogo]) {
         numeroDeAcertos +=1
+        numerosAcertados.push(numerosSorteados[indexSorteados])
        }
     }
 }
-console.log(`acertos ${numeroDeAcertos}`)
-console.log(`meu jogo ${meuJogo}`)
-console.log(`Numeros Sorteados ${numerosSorteados}`)
+//console.log(`acertos ${numeroDeAcertos}`)
+//console.log(`meu jogo ${meuJogo}`)
+//console.log(`Numeros Sorteados: ${numerosSorteados}`)
+//console.log(`Numeros quer acertei: ${numerosAcertados}`)
+let megaSena = [];
+async function main() {
+    const result = await fetch('https://loteriascaixa-api.herokuapp.com/api/megasena/latest')
+    const data = await result.json()
+    megaSena.push(data.dezenas)
 
+    console.log(megaSena)
+    
+}
+main()
 
 //parou em 1:19
 //testando git
