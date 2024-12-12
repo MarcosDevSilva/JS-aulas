1da2271cf53796a8e97521fd7d20d5ab284a8108
Marco <tassio50000@gmail.com>
2024-12-11 21:25:36 -0300

    fazendo comparação de acertos

--- megaSena.js
+++ megaSena.js
@@ -1,5 +1,6 @@
 let numerosSorteados = [];
-let meuJogo =[];
+let meuJogo =[5,10,35,48,29,19];
+let numeroDeAcertos = 0;
 
 
 for (let index = 0; index < 6; index += 1 ){
@@ -10,7 +11,18 @@ for (let index = 0; index < 6; index += 1 ){
         index -=1;
     }
 }
-console.log(numerosSorteados)
+
+for (let indexSorteados = 0; indexSorteados < numerosSorteados.length; indexSorteados +=1) {
+    for ( let indexMeujogo = 0; indexMeujogo < meuJogo.length; indexMeujogo +=1){
+       if (numerosSorteados[indexSorteados] === meuJogo[indexMeujogo]) {
+        numeroDeAcertos +=1
+       }
+    }
+}
+console.log(`acertos ${numeroDeAcertos}`)
+console.log(`meu jogo ${meuJogo}`)
+console.log(`Numeros Sorteados ${numerosSorteados}`)
+
 
 //parou em 1:19
 //testando git
